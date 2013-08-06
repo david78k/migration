8b82c3d474292df339f0dec23b298a2a3a657927
root <root@c11node9.(none)>
2013-08-06 15:14:09 -0400

    precopy.m

--- matlab/mem-change-pdf.m
+++ matlab/mem-change-pdf.m
@@ -6,8 +6,23 @@ r = 8*8
 b = 1
 % network(?) error rate (x100%)
 err = 0.001
-% delay (sec)
+% processing delay (sec)
 d = 0.5
+% average compression rate (%)
+c = 0.5
 
-% memory trasfer time
-td = r*(1 + err)/b + d
+% downtime: memory transfer time
+td = r*c*(1 + err)/b + d
+
+% transferred part of memory to destination
+rctmp = b*(td - d)/(c*(1 + err))
+rc = min (max (rctmp, 0), r)
+
+% remaining parts of memory 
+rr = r - rc
+
+% amount of memory to be recopied
+d(rrc)/d(t) = w*rc
+
+% memory changing rate
+w = 
