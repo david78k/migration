616043c4fcb4f5c3d8cd7182883c0c6d9db54f9f
root <root@rapidmigrate.com>
2013-09-11 21:31:46 -0400

    multibar.R

--- R/multibar.R
+++ R/multibar.R
@@ -1,8 +1,12 @@
 library(ggplot2)
+
 # header = TRUE ignores the first line, check.names = FALSE allows '+' in 'C++'
 benchmark <- read.table("data.dat", header = TRUE, row.names = "Title", check.names = FALSE)
+
 # 't()' is matrix tranposition, 'beside = TRUE' separates the benchmarks, 'heat' provides nice colors
 barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(4))
+
 # 'cex' stands for 'character expansion', 'bty' for 'box type' (we don't want borders)
 legend("topleft", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(4))
 
+ggsave("plot.png",width=4,height=4,dpi=100)
