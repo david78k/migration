943bd1de2f0caf79be0dda871a3f45efd3689a46
root <root@rapidmigrate.com>
2013-09-12 11:41:19 -0400

    180ms.R

--- R/180ms.R
+++ R/180ms.R
@@ -10,8 +10,8 @@ png("180ms.png")
 benchmark <- read.table("180ms.dat", header = TRUE, row.names = "vwnd", check.names = FALSE)
 
 # 't()' is matrix tranposition, 'beside = TRUE' separates the benchmarks, 'heat' provides nice colors
-barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(4))
+barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(6))
 
 # 'cex' stands for 'character expansion', 'bty' for 'box type' (we don't want borders)
-legend("topright", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(4))
+legend("topright", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(6))
 
