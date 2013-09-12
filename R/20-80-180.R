63fe7b1980610d509624b6b249dd83452b8d73d9
root <root@rapidmigrate.com>
2013-09-12 12:43:04 -0400

    20-80-180.R

---
+++ R/20-80-180.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+
+require(devEMF)
+emf('20-80-180.emf')
+
+#png("20-80-180.emf")
+#postscript("plot.eps")
+#emf("plot.emf")
+
+# header = TRUE ignores the first line, check.names = FALSE allows '+' in 'C++'
+benchmark <- read.table("20-80-180.dat", header = TRUE, row.names = "vwnd", check.names = FALSE)
+
+# 't()' is matrix tranposition, 'beside = TRUE' separates the benchmarks, 'heat' provides nice colors
+barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(6))
+
+# 'cex' stands for 'character expansion', 'bty' for 'box type' (we don't want borders)
+legend("topright", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(6))
+
