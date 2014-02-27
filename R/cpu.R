5be68e8b309c553aec495d8985b9af33d0a86baf
root <root@rapidmigrate.com>
2014-02-27 00:26:48 -0500

    cpu.R

--- R/cpu.R
+++ R/cpu.R
@@ -18,17 +18,17 @@ emf('cpu.emf')
 
 #cpus <- read.table("cpu.dat", header = TRUE, row.name = "time")
 cpus <- scan("cpu.dat", skip=1)
-cpus
+#cpus
 
 #plot(cpus)
-hist(cpus)
+#hist(cpus)
 
 # 't()' is matrix tranposition, 'beside = TRUE' separates the benchmarks, 'heat' provides nice colors
 #barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(6))
 #barplot((as.matrix(benchmark)), beside = TRUE, col = heat.colors(6), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
 #barplot(benchmark, beside = TRUE, xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
 #barplot((as.matrix(benchmark)), col = heat.colors(6), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
-#barplot((as.matrix(benchmark)), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
+barplot((as.matrix(benchmark)), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
 
 # 'cex' stands for 'character expansion', 'bty' for 'box type' (we don't want borders)
 #legend("topright", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(6))
