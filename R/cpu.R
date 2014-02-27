ac7e16fda60d2ae67ece03be7ce5f810ae10573f
root <root@rapidmigrate.com>
2014-02-26 20:48:33 -0500

    cpu.R

--- R/cpu.R
+++ R/cpu.R
@@ -12,7 +12,7 @@ benchmark <- read.table("cpu.dat", header = TRUE, row.names = "vwnd", check.name
 
 # 't()' is matrix tranposition, 'beside = TRUE' separates the benchmarks, 'heat' provides nice colors
 #barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(6))
-barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(6), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
+barplot((as.matrix(benchmark)), beside = TRUE, col = heat.colors(6), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
 
 # 'cex' stands for 'character expansion', 'bty' for 'box type' (we don't want borders)
 legend("topright", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(6))
