4742b402062fe415132fb2ff27d6bfd95f1976b6
root <root@rapidmigrate.com>
2014-02-26 20:49:50 -0500

    cpu.R

--- R/cpu.R
+++ R/cpu.R
@@ -15,5 +15,5 @@ benchmark <- read.table("cpu.dat", header = TRUE, row.names = "vwnd", check.name
 barplot((as.matrix(benchmark)), beside = TRUE, col = heat.colors(6), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
 
 # 'cex' stands for 'character expansion', 'bty' for 'box type' (we don't want borders)
-legend("topright", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(6))
+#legend("topright", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(6))
 
