23073e979050749b801733858aba4449db2fd720
root <root@rapidmigrate.com>
2014-02-27 01:35:51 -0500

    all

---
+++ log/idle/postcopy/src.c11node9.r1.R
@@ -0,0 +1,44 @@
+library(ggplot2)
+
+require(devEMF)
+emf('../log/idle/postcopy/src.c11node9.r1.emf')
+
+#png("../log/idle/postcopy/src.c11node9.r1.emf")
+#postscript("plot.eps")
+#emf("plot.emf")
+
+#cpus <- scan (pipe("awk '{print }' ../log/idle/postcopy/src.c11node9.r1.cpu"), skip=2)
+#cpus <- read.table("../log/idle/postcopy/src.c11node9.r1.cpu", colClasses=3)
+
+# header = TRUE ignores the first line, check.names = FALSE allows '+' in 'C++'
+#benchmark <- read.table("../log/idle/postcopy/src.c11node9.r1.cpu", header = TRUE, row.names = "time", check.names = FALSE)
+#benchmark <- read.table("../log/idle/postcopy/src.c11node9.r1.cpu", check.names = FALSE, comment.char = "#")
+#benchmark <- read.table("../log/idle/postcopy/src.c11node9.r1.cpu", header = TRUE, check.names = FALSE, comment.char = "#")
+#benchmark <- read.table("../log/idle/postcopy/src.c11node9.r1.cpu", check.names = FALSE)
+
+#cpus <- read.table("../log/idle/postcopy/src.c11node9.r1.cpu", header = TRUE, row.name = "time")
+cpus <- read.table("../log/idle/postcopy/src.c11node9.r1.cpu", skip=2)
+#cpus <- scan("../log/idle/postcopy/src.c11node9.r1.cpu", skip=1)
+#cpus <- read.csv("../log/idle/postcopy/src.c11node9.r1.cpu", sep=",", head=TRUE)
+#print(cpus)
+#names(cpus)
+
+#stripchart(cpus)
+plot(cpus)
+#hist(cpus)
+#boxplot(cpus)
+
+# 't()' is matrix tranposition, 'beside = TRUE' separates the benchmarks, 'heat' provides nice colors
+#barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(6))
+#barplot((as.matrix(benchmark)), beside = TRUE, col = heat.colors(6), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
+#barplot(benchmark, beside = TRUE, xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
+#barplot((as.matrix(benchmark)), col = heat.colors(6), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
+#barplot((as.matrix(benchmark)), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
+
+#barplot((as.matrix(cpus)), beside = TRUE, xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
+barplot(t(as.matrix(cpus)), xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
+#barplot(cpus, xlab = "TIME (SEC)", ylab = "CPU USAGE (%)")
+
+# 'cex' stands for 'character expansion', 'bty' for 'box type' (we don't want borders)
+#legend("topright", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(6))
+
