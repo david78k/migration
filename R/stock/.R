9c73eebdaa6e18b10448f0bac66629263d141db9
root <root@rapidmigrate.com>
2014-03-22 13:33:57 -0400

    .R

---
+++ R/stock/.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+
+require(devEMF)
+emf('.emf')
+
+aapl <- read.csv("http://www.google.com/finance/historical?q=NASDAQ:AAPL&authuser=0&output=csv ", sep=",", header=1)
+
+#print(aapl)
+#print(aapl.google)
+
+#write.csv(aapl, file = "appl.csv")
+#write.table(aapl, file = "appl.tab")
+
+plot(appl)
+
+# header = TRUE ignores the first line, check.names = FALSE allows '+' in 'C++'
+#benchmark <- read.table("", header = TRUE, row.names = "vwnd", check.names = FALSE)
+
+# 't()' is matrix tranposition, 'beside = TRUE' separates the benchmarks, 'heat' provides nice colors
+#barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(6))
+#barplot(t(as.matrix(benchmark)), beside = TRUE, col = heat.colors(6), xlab = "TIME (DAY)", ylab = "STOCK PRICE")
+
+# 'cex' stands for 'character expansion', 'bty' for 'box type' (we don't want borders)
+#legend("topright", names(benchmark), cex = 0.9, bty = "n", fill = heat.colors(6))
+
--- R/stock/stock.R
+++ R/stock/stock.R
@@ -2,11 +2,12 @@
 
 #aapl.google <- read.csv("http://www.google.com/finance/historical?q=NASDAQ:AAPL&authuser=0&output=csv ", sep=",", header=1)
 aapl <- read.csv("http://www.google.com/finance/historical?q=NASDAQ:AAPL&authuser=0&output=csv ", sep=",", header=1)
-#aapl <- read.csv("http://www.google.com/finance/historical?q=NASDAQ:AAPL&authuser=0&output=csv ", sep="\t", header=1)
 
-print(aapl)
+#print(aapl)
 #print(aapl.google)
 
-write.csv(aapl, file = "appl.csv")
-write.table(aapl, file = "appl.tab")
+#write.csv(aapl, file = "appl.csv")
+#write.table(aapl, file = "appl.tab")
 #write.csv(aapl.google, file = "aapl.csv")
+
+plot(appl)
