dec78b27bd41bf2e1f3d7a534d9c922bdad78a57
root <root@rapidmigrate.com>
2014-05-27 21:53:49 -0400

    all

--- plot/plotnet.m
+++ plot/plotnet.m
@@ -80,12 +80,14 @@ set(findall(gcf,'type','text'),'FontSize',fontsize)
 
 %ylabh = get(gca,'YLabel');
 %set(ylabh, 'Units', 'Normalized', 'Position', [-0.1, 0.5, 0]);
-set(get(gca, 'YLabel'), 'Position', [-2, 5, 0]);
+%%set(get(gca, 'YLabel'), 'Position', [-2, 5, 0]);
 %set(get(gca, 'YLabel'), 'Units', 'Normalized', 'Position', [-0.2, 0.5, 0]);
 %set(get(gca, 'YLabel'), 'Units', 'Normalized', 'Position', [-1, 1, 0]);
 
-xlabh = get(gca,'XLabel');
-set(xlabh,'Position',get(xlabh,'Position') - [0 .2 0])
+%xlabh = get(gca,'XLabel');
+%set(xlabh,'Position',get(xlabh,'Position') - [0 .2 0])
+ylabh = get(gca,'YLabel');
+set(ylabh,'Position',get(ylabh,'Position') - [0.5 0.5 0])
 
 saveas (1, strcat(output, ".png"));
 saveas (1, strcat(output, ".eps"));
