fef6d82e1001e81e10785c36cd7f3588bcff79ba
root <root@rapidmigrate.com>
2014-05-28 15:56:56 -0400

    all

--- plot/plotnet.R
+++ plot/plotnet.R
@@ -47,12 +47,13 @@ genplot <- function (type) {
 		#emf('aapl.emf')
 	}
 
-	# margins: 3 lines of outer margin, 4 lines of inside margin
+	# margins: oma for the number of lines in outer margin, mar for the number of lines in inside margin
 	# c(bottom, left, top, right)
 	par(oma=c(0,0,0,0))               
 	#par(oma=c(1,1,1,1))               # Set outer margin areas (only necessary in order to plot extra y-axis)
-	par(mar=c(6,6,0,0))
 	#par(mar=c(5,5,0,0)) # good
+	par(mar=c(4,4,0,0))
+	#par(mar=c(6,6,0,0)) # bit wide
 
 	# aggregate throughput
 	#matplot(aapl[,1], aapl[,5], type = "l", col="red")
