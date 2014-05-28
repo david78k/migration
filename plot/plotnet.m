0dddcac6b6b08a8293ed227ad62187986eb3d533
root <root@rapidmigrate.com>
2014-05-27 20:01:49 -0400

    all

--- plot/plotnet.m
+++ plot/plotnet.m
@@ -11,7 +11,7 @@ startrow = 7;
 recvcol = 8;
 sendcol = 9;
 
-fontsize = 14;
+fontsize = 20;
 
 %prefix = "rand-6-r2"
 data = strcat(prefix, postfix)
@@ -48,6 +48,12 @@ plot(x, A(:,1)/1000000, x, A(:,1)/(1000000*N), ':');
 xlabel('TIME (SEC)');
 ylabel('THROUGHPUT (MB/s)');
 legend('AGGREGATE', 'PER VM');
+
+%figurehandle = gcf;
+%set(findall(figureHandle,'type','text'),'fontSize',fontsize,'fontWeight','bold')
+
+set(gca,'FontSize',30,'fontWeight','bold')
+set(findall(gcf,'type','text'),'FontSize',30,'fontWeight','bold')
 
 saveas (1, strcat(output, ".png"));
 saveas (1, strcat(output, ".eps"));
