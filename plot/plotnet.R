55c42cbf5dd32e82b30d8a788bf7dd77064c1193
root <root@rapidmigrate.com>
2014-05-28 16:18:38 -0400

    all

--- plot/plotnet.R
+++ plot/plotnet.R
@@ -49,7 +49,7 @@ genplot <- function (type) {
 
 	# margins: oma for the number of lines in outer margin, mar for the number of lines in inside margin
 	# c(bottom, left, top, right)
-	#par(oma=c(0,0,0,0))               
+	par(oma=c(0,0,0,0))               
 	#par(oma=c(1,1,1,1))               # Set outer margin areas (only necessary in order to plot extra y-axis)
 	#par(mar=c(5,5,0,0)) # good but bottom is a bit wide
 	par(mar=c(4,5,0,0))  # perfect, both tight
