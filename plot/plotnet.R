3f40bd70aa8870ec3b46ea5a53b339fc369dd083
root <root@rapidmigrate.com>
2014-05-28 15:47:46 -0400

    all

--- plot/plotnet.R
+++ plot/plotnet.R
@@ -51,7 +51,7 @@ genplot <- function (type) {
 	# c(bottom, left, top, right)
 	par(oma=c(0,0,0,0))               
 	#par(oma=c(1,1,1,1))               # Set outer margin areas (only necessary in order to plot extra y-axis)
-	par(mar=c(7,7,0,0))
+	par(mar=c(5,4,0,0))
 
 	# aggregate throughput
 	#matplot(aapl[,1], aapl[,5], type = "l", col="red")
