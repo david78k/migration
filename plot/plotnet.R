1fadabd556a03dab22d5df7ab5b9c836b5c479fd
root <root@rapidmigrate.com>
2014-05-29 03:17:54 -0400

    all

--- plot/plotnet.R
+++ plot/plotnet.R
@@ -26,19 +26,24 @@ secondlty = 5 # best
 #secondlty = 2 # good 
 #secondlty = 3 # too pale
 
-#fontsize = 1.5 # works
-#fontsize = 1.6 # a little bit big
-#fontsize = 1.7 # big
-#fontsize = 1.8 # too big
-fontsize = 2 # too big, outside the box
+# cex
+fontsize = 1.5 
+#fontsize = 2 
 
-library(ggplot2)
+#library(ggplot2)
 require(devEMF)
+
+#aapl <- read.csv("http://www.google.com/finance/historical?q=NASDAQ:AAPL&authuser=0&output=csv ", sep=",", header=1)
+data <- read.csv(src, sep=",", skip = startrow, header=1)
+#aapl = aapl[nrow(aapl):1, ]
+#print(aapl)
 
 genplot <- function (type) {
 	if(type == "png") {
 		#type(paste0(prefix, sep = ".", type))
 		png(paste0(prefix, ".png"))
+	} else if (type == "pdf") {
+		pdf(paste0(prefix, ".pdf"))
 	} else if (type == "eps") {
 		#png("aapl.png")
 		#postscript(paste0(prefix, ".eps"), res = resolution)
@@ -56,31 +61,31 @@ genplot <- function (type) {
 rm(list = ls())      # Clear all variables
 graphics.off()    # Close graphics windows
 
-pdf("examaple.pdf")
-#png("example.png")
-#postscript("example.eps")
-
 # Generate sample time series data
-ti = 1:50                                   # Generate 50 sample time steps
-y1 = 8 + rnorm(50)                          # Generate 50 stochastic data points for time series y1
-y2 = seq(10,15,length.out=50) + rnorm(50)   # Generate 50 stochastic data points for time series y2
-y3 = seq(80,110,length.out=50) + rnorm(50)  # Generate 50 stochastic data points for time series y3
+#ti = 1:50                                   # Generate 50 sample time steps
+#y1 = 8 + rnorm(50)                          # Generate 50 stochastic data points for time series y1
+#y2 = seq(10,15,length.out=50) + rnorm(50)   # Generate 50 stochastic data points for time series y2
+#y3 = seq(80,110,length.out=50) + rnorm(50)  # Generate 50 stochastic data points for time series y3
 
 # Plot the y1 data
 par(oma=c(2,2,2,4))               # Set outer margin areas (only necessary in order to plot extra y-axis)
 
-plot(ti, y1,                      # Data to plot - x, y
+plot(data[,startcol]/1000000.0/N,            # Data to plot - x, y
      type="b",                    # Plot lines and points. Use "p" for points only, "l" for lines only
-     main="Time series plot",     # Main title for the plot
-     xlab="Time",                 # Label for the x-axis
-     ylab="Response (y1 & y2)",   # Label for the y-axis
+#     main="Time series plot",     # Main title for the plot
+     xlab="TIME (SEC)",                 # Label for the x-axis
+     ylab="THROUGHPUT (MB/S)",   # Label for the y-axis
      font.lab=2,                  # Font to use for the axis labels: 1=plain text, 2=bold, 3=italic, 4=bold italic
+     cex.axis = fontsize,
+     cex.lab = fontsize,
      ylim=c(0,20),                # Range for the y-axis; "xlim" does same for x-axis
      xaxp=c(0,50,5),              # X-axis min, max and number of intervals; "yaxp" does same for y-axis
-     bty="l")                     # Box around plot to contain only left and lower lines
+#     bty="l",                     # Box around plot to contain only left and lower lines
+     las = 1                      # labels are parallel (=0) or perpendicular(=2) to axis, 1 for x-axis = 0 and y-axis = 1
+)
 
 # Add y2 data to the same plot
-points(ti, y2,
+points(data[,startcol]/1000000.0/N,
        type="b",                  # Plot lines and points
        lty=1,                     # Line type: 0=blank, 1=solid, 2=dashed, 3=dotted, 4=dotdash, 5=longdash, 6=twodash
        lwd=1,                     # Line width
@@ -88,96 +93,64 @@ points(ti, y2,
        col="red")                 # Color of the plotted data
 
 # Add y3 data to the same plot, but on a different axis
-par(new=T,                        # The next high-level plotting command (actually plot.new) should not clean the frame before drawing ìas if it was on a new deviceî.
-    oma=c(2,2,2,4))               # Increase the size of the outer margins to accomodate second y axis
+#par(new=T,                        # The next high-level plotting command (actually plot.new) should not clean the frame before drawing ìas if it was on a new deviceî.
+#    oma=c(2,2,2,4))               # Increase the size of the outer margins to accomodate second y axis
 
-plot(ti, y3,
-       yaxt="n",                  # Do not plot the y-axis
-       ylab="",                   # Do not plot the y-axis label
-       xlab="",                   # Do not plot the x-axis label
-       type="b",                  # Plot lines and points
-       lty=1,                     # Line type: 0=blank, 1=solid, 2=dashed, 3=dotted, 4=dotdash, 5=longdash, 6=twodash
-       lwd=1,                     # Line width
-       pch=19,                    # Point type: pch=19 - solid circle, pch=20 - bullet (smaller circle), pch=21 - circle, pch=22 - square, pch=23 - diamond, pch=24 - triangle point-up, pch=25 - triangle point down.
-       col="blue")                # Color of the plotted data
+#plot(ti, y3,
+#       yaxt="n",                  # Do not plot the y-axis
+#       ylab="",                   # Do not plot the y-axis label
+#       xlab="",                   # Do not plot the x-axis label
+#       type="b",                  # Plot lines and points
+#       lty=1,                     # Line type: 0=blank, 1=solid, 2=dashed, 3=dotted, 4=dotdash, 5=longdash, 6=twodash
+#       lwd=1,                     # Line width
+#       pch=19,                    # Point type: pch=19 - solid circle, pch=20 - bullet (smaller circle), pch=21 - circle, pch=22 - square, pch=23 - diamond, pch=24 - triangle point-up, pch=25 - triangle point down.
+#       col="blue")                # Color of the plotted data
 
-axis(4,                           # Add a second axis: 1=below, 2=left, 3=above and 4=right
-    pretty(range(y3),10))         # Intervals for the second y-axis
+#axis(4,                           # Add a second axis: 1=below, 2=left, 3=above and 4=right
+#    pretty(range(y3),10))         # Intervals for the second y-axis
 
-mtext("Response (y3)",            # Add second y-axis label
-      side=4,                     # Add to right hand side of plot
-      line=3,                     # Add to line 3 from the margin
-      font=2)                     # Print label in bold
+#mtext("Response (y3)",            # Add second y-axis label
+#      side=4,                     # Add to right hand side of plot
+#      line=3,                     # Add to line 3 from the margin
+#      font=2)                     # Print label in bold
 
 # Add a legend to the plot
 legend("topleft",                       # x-y coordinates for location of the legend
-       legend=c("y1", "y2", "y3"),      # Legend labels
-       col=c("black", "red", "blue"),   # Color of points or lines
-       pch=c(21,19,19),                 # Point type
-       lty=c(1,1,1),                    # Line type
-       lwd=c(1,1,1),                    # Line width
-       title="Time series")             # Legend title
-
+       legend=c("y1", "y2"),      # Legend labels
+       col=c("black", "red"),   # Color of points or lines
+       pch=c(21,19),                 # Point type
+       lty=c(1,1),                    # Line type
+       lwd=c(1,1),                    # Line width
+       cex = fontsize
+)
 	# margins: oma for the number of lines in outer margin, mar for the number of lines in inside margin
 	# c(bottom, left, top, right)
-	par(oma=c(0,0,0,0))               # Set outer margin areas (only necessary in order to plot extra y-axis)
-	par(mar=c(5,5,0,0)) # good but bottom is a bit wide
+	#par(oma=c(0,0,0,0))               # Set outer margin areas (only necessary in order to plot extra y-axis)
+	#par(mar=c(5,5,0,0)) # good but bottom is a bit wide
 	#par(mar=c(4,5,0,0))  # perfect, both tight
 	#par(mar=c(4,6,0,0)) # bottom good, left wide
 	#par(mar=c(6,6,0,0)) # bit wide
 
 	# aggregate throughput
 	#matplot(aapl[,1], aapl[,5], type = "l", col="red")
-	plot(data[,startcol]/1000000.0, xlab = "TIME (SEC)", ylab = "THROUGHPUT (MB/S)", type = "l", col="blue", cex.axis = fontsize, cex.lab = fontsize, lwd = linewidth)
+	#plot(data[,startcol]/1000000.0, xlab = "TIME (SEC)", ylab = "THROUGHPUT (MB/S)", type = "l", col="blue", cex.axis = fontsize, cex.lab = fontsize, lwd = linewidth)
 	#plot(aapl[,5], xlab = "TIME", ylab = "PRICE ($)", type = "l", col="blue", cex.axis = fontsize, cex.lab = fontsize)
 
 	# per VM. lty 5 for longdash, lty 2 for dashed
-	lines(data[,startcol]/1000000.0/N, type = "l", lty = secondlty, col=secondlc, lwd = linewidth)
+	#lines(data[,startcol]/1000000.0/N, type = "l", lty = secondlty, col=secondlc, lwd = linewidth)
 
 	# open value
 	#lines(aapl[,2], type = "l", col="red")
 	
-	names <- c("AGGREGATE", "PER VM")
+	#names <- c("AGGREGATE", "PER VM")
 	# 'cex' stands for 'character expansion', 'bty' for 'box type' (we don't want borders)
 	# lty for line types, lwd for line width
-	legend("center", names, lty = c(1, secondlty), col=c("blue", secondlc), lwd = c(linewidth, linewidth), bty = 'n',  cex = fontsize)
+	#legend("center", names, lty = c(1, secondlty), col=c("blue", secondlc), lwd = c(linewidth, linewidth), bty = 'n',  cex = fontsize)
 	#legend("topright", names, lty = 1, bty = 'n',  cex = 1.5)
 }
 
-#aapl <- read.csv("http://www.google.com/finance/historical?q=NASDAQ:AAPL&authuser=0&output=csv ", sep=",", header=1)
-data <- read.csv(src, sep=",", skip = startrow, header=1)
-#aapl = aapl[, startcol]
-#aapl = aapl[nrow(aapl):1, ]
-
-#print(aapl)
-
 genplot("png")
-genplot("eps")
+genplot("pdf")
+#genplot("eps")
 genplot("emf")
 
-# barplot
-#emf('1.dest.emf')
-
-#png("../log/apachebench/postcopy/1.dest.emf")
-#postscript("plot.eps")
-#emf("plot.emf")
-
-# header = TRUE ignores the first line, check.names = FALSE allows '+' in 'C++'
-#benchmark <- read.table("../log/apachebench/postcopy/1.dest.dstat", header = TRUE, row.names = "time", check.names = FALSE)
-#cpus <- read.table(pipe("awk '{print $3}' ../log/apachebench/postcopy/1.dest.dstat | sed '1,2d'"))
-##cpus <- read.table(pipe("awk '{print $3}' ../log/apachebench/postcopy/1.dest.dstat"), skip=3, nrows=27)
-#cpus <- scan("../log/apachebench/postcopy/1.dest.dstat", skip=1)
-#cpus <- read.csv("../log/apachebench/postcopy/1.dest.dstat", sep=",", head=TRUE)
-#print(cpus)
-#names(cpus)
-
-#stripchart(cpus)
-#plot(cpus)
-#hist(cpus)
-#boxplot(cpus)
-
-# 't()' is matrix tranposition, 'beside = TRUE' separates the benchmarks, 'heat' provides nice colors
-#barplot((as.matrix(cpus)), beside = TRUE, xlab = "TIME (SEC)", ylab = "CPU UTILIZATION (%)")
-##barplot(t(as.matrix(metrics)), cex.axis = 1.5, cex.lab = 1.5, cex.names = 1.5, xlab = "TIME (SEC)", ylab = "BANDWIDTH UTILIZATION (MB/s)")
-#barplot(t(100 - as.matrix(cpus)), ylim = c(0, 100), cex.axis = 1.5, cex.lab = 1.5, cex.names = 1.5, xlab = "TIME (SEC)", ylab = "CPU UTILIZATION (%)")
-#barplot(cpus, xlab = "TIME (SEC)", ylab = "CPU UTILIZATION (%)")
