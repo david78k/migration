acbfe92c1a89cded09e4b9e9ad120c51daa06578
root <root@rapidmigrate.com>
2014-05-30 18:21:59 -0400

    all

--- plot/plotnet.R
+++ plot/plotnet.R
@@ -30,8 +30,8 @@ linewidth = 2.5
 secondlc = "red"
 
 # second line type: 2 for dashed, 3 for dotted, 4 for dotdash, 5 for longdash, 6 for twodash
-secondlty = 5 # best
-#secondlty = 2 # good 
+#secondlty = 5 # best
+secondlty = 2 # good 
 #secondlty = 3 # too pale
 
 legendpos = "bottom"
